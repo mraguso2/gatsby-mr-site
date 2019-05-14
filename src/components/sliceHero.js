be39b57f13bbae876657853712bda0c721bacd4f
Michael Raguso <michael.raguso@gmail.com>
2019-05-13 22:53:20 -0400

    add slice animation to letter W - hacked, could be better

--- src/components/sliceHero.js
+++ src/components/sliceHero.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styled, { keyframes } from 'styled-components';
 
 import ellipseBackground from '../images/ellipse-bkgd.svg';
-import pieSliced from '../images/Pie-Sliced.svg';
+import pieSlicedM from '../images/Pie-Sliced-M.svg';
+import pieSlicedW from '../images/Pie-Sliced-W.svg';
 
 const HeroStyled = styled.div`
   text-align: center;
@@ -15,33 +17,15 @@ const ImgStyled = styled.img`
   left: 0;
   right: 0;
   margin: auto;
+  height: 160px;
 `;
-// height: 160px;
 
 const PieContainer = styled.div`
   position: relative;
   display: inline-block;
 `;
 
-// const rotateAndTranslate = keyframes`
-//   0% {
-//     opacity: 0; transform: translate3d(0, 0, 0) rotate(0deg);
-//   }
-//   10% {
-//     opacity: 1;
-//   }
-//   50% {
-//     transform: translate3d(10px, -25px, 0) rotate(20deg);
-//   }
-//   90% {
-//     opacity: 1;
-//   }
-//   100% {
-//     opacity: 0; transform: translate3d(0, 0, 0) rotate(0deg);
-//   }
-// `;
-
-const rotateAndTranslate = keyframes`
+const moveSliceM = keyframes`
   0% {
     opacity: 0; transform: translate3d(0, 0, 0) rotate(0deg);
   }
@@ -53,16 +37,28 @@ const rotateAndTranslate = keyframes`
   }
 `;
 
+const moveSliceW = keyframes`
+  0% {
+    opacity: 0; transform: translate3d(0, 0, 0) rotate(10deg);
+  }
+  10% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 1; transform: translate3d(-10px, 20px, 0) rotate(40deg);
+  }
+`;
+
 const PieImage = styled.img`
   position: absolute;
   left: 0;
   right: 0;
+  opacity: 0;
   margin: auto;
   bottom: 0;
-  top: 0;
-
+  top: ${props => (props.letter === 'M' ? '0' : 'inherit')};
   transition: all 1s;
-  animation: ${rotateAndTranslate} 1s linear 1 forwards;
+  animation: ${props => (props.letter === 'M' ? moveSliceM : moveSliceW)} 1s linear 1s 1 forwards;
 `;
 
 // const PieImage = styled(ImgStyled)`
@@ -78,9 +74,18 @@ const PieImage = styled.img`
 
 const EllipseBackground = () => <ImgStyled src={ellipseBackground} alt="Background Image" />;
 
-const LetterM = () => (
-  <PieContainer>
-    <PieImage src={pieSliced} alt="Slice of Pie" />M
+const handlePieClick = e => {
+  const pieContainer = e.currentTarget;
+  const slice = pieContainer.querySelector('img');
+  console.log(slice);
+  const seconds = slice.cloneNode(true);
+  pieContainer.replaceChild(seconds, slice);
+};
+
+const LetterPieSlice = ({ letter }) => (
+  <PieContainer onClick={e => handlePieClick(e)}>
+    <PieImage letter={letter} src={letter === 'M' ? pieSlicedM : pieSlicedW} alt="Slice of Pie" />
+    {letter}
   </PieContainer>
 );
 
@@ -88,11 +93,16 @@ const SliceHero = () => (
   <div>
     <EllipseBackground />
     <HeroStyled>
-      Welcome to <LetterM />y <br />
+      Welcome to <LetterPieSlice letter="M" />y <br />
       Slice of the <br />
-      <span>W</span>eb
+      <LetterPieSlice letter="W" />
+      eb
     </HeroStyled>
   </div>
 );
+
+LetterPieSlice.propTypes = {
+  letter: PropTypes.string.isRequired
+};
 
 export default SliceHero;
